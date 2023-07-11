eccd6dab913239d6fe178d95d9d9bb82c9b3dbad
shrey parekh <your_email@example.com>
2023-07-11 13:11:30 +0530

    added title and include  roboto font

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,9 @@ function App() {
 
   return (
     <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
